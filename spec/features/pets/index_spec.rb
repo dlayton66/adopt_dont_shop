49f016c6161bd4ef7b259b68e5a9c3fe5c6e876a
Drew Layton <dlayton66@gmail.com>
2022-12-20 10:31:24 -0700

    Make pet index tests fit new changes

--- spec/features/pets/index_spec.rb
+++ spec/features/pets/index_spec.rb
@@ -1,114 +1,87 @@
 require 'rails_helper'
 
 RSpec.describe 'the pets index' do
-  it 'lists all the pets with their attributes' do
-    shelter = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
-    pet_1 = Pet.create(adoptable: true, age: 1, breed: 'sphynx', name: 'Lucille Bald', shelter_id: shelter.id)
-    pet_2 = Pet.create(adoptable: true, age: 3, breed: 'doberman', name: 'Lobster', shelter_id: shelter.id)
+  before :each do
+    @shelter = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9) 
 
+    @pet_1 = Pet.create(adoptable: true, age: 7, breed: 'sphynx', name: 'Bare-y Manilow', shelter_id: @shelter.id)
+    @pet_2 = Pet.create(adoptable: true, age: 3, breed: 'domestic pig', name: 'Babe', shelter_id: @shelter.id)
+    @pet_3 = Pet.create(adoptable: false, age: 4, breed: 'chihuahua', name: 'Elle', shelter_id: @shelter.id)
+  end
+
+  it 'lists all the pets with their attributes' do
     visit "/pets"
 
-    expect(page).to have_content(pet_1.name)
-    expect(page).to have_content(pet_1.breed)
-    expect(page).to have_content(pet_1.age)
-    expect(page).to have_content(shelter.name)
+    expect(page).to have_content(@pet_1.name)
+    expect(page).to have_content(@pet_1.breed)
+    expect(page).to have_content(@pet_1.age)
+    expect(page).to have_content(@shelter.name)
 
-    expect(page).to have_content(pet_2.name)
-    expect(page).to have_content(pet_2.breed)
-    expect(page).to have_content(pet_2.age)
-    expect(page).to have_content(shelter.name)
+    expect(page).to have_content(@pet_2.name)
+    expect(page).to have_content(@pet_2.breed)
+    expect(page).to have_content(@pet_2.age)
+    expect(page).to have_content(@shelter.name)
   end
 
   it 'only lists adoptable pets' do
-    shelter = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
-    pet_1 = Pet.create(adoptable: true, age: 1, breed: 'sphynx', name: 'Lucille Bald', shelter_id: shelter.id)
-    pet_2 = Pet.create(adoptable: true, age: 3, breed: 'doberman', name: 'Lobster', shelter_id: shelter.id)
-    pet_3 = Pet.create(adoptable: false, age: 2, breed: 'saint bernard', name: 'Beethoven', shelter_id: shelter.id)
-
     visit "/pets"
 
-    expect(page).to_not have_content(pet_3.name)
+    expect(page).to_not have_content(@pet_3.name)
   end
 
   it 'displays a link to edit each pet' do
-    shelter = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
-    pet_1 = Pet.create(adoptable: true, age: 1, breed: 'sphynx', name: 'Lucille Bald', shelter_id: shelter.id)
-    pet_2 = Pet.create(adoptable: true, age: 3, breed: 'doberman', name: 'Lobster', shelter_id: shelter.id)
-
     visit '/pets'
 
-    expect(page).to have_content("Edit #{pet_1.name}")
-    expect(page).to have_content("Edit #{pet_2.name}")
+    within("#pet-#{@pet_1.id}") do
+      expect(page).to have_button("Edit")
+    end
+    within("#pet-#{@pet_2.id}") do
+      expect(page).to have_button("Edit")
+      click_button("Edit")
+    end
 
-    click_link("Edit #{pet_1.name}")
-
-    expect(page).to have_current_path("/pets/#{pet_1.id}/edit")
+    expect(page).to have_current_path("/pets/#{@pet_2.id}/edit")
   end
 
   it 'displays a link to delete each pet' do
-    shelter = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
-    pet_1 = Pet.create(adoptable: true, age: 1, breed: 'sphynx', name: 'Lucille Bald', shelter_id: shelter.id)
-    pet_2 = Pet.create(adoptable: true, age: 3, breed: 'doberman', name: 'Lobster', shelter_id: shelter.id)
-
     visit '/pets'
 
-    expect(page).to have_content("Delete #{pet_1.name}")
-    expect(page).to have_content("Delete #{pet_2.name}")
-
-    click_link("Delete #{pet_1.name}")
+    within("#pet-#{@pet_1.id}") do
+      expect(page).to have_button("Delete")
+    end
+    within("#pet-#{@pet_2.id}") do
+      expect(page).to have_button("Delete")
+      click_button("Delete")
+    end
 
     expect(page).to have_current_path("/pets")
-    expect(page).to_not have_content(pet_1.name)
+    expect(page).to_not have_content(@pet_2.name)
   end
 
   it 'has a text box to filter results by keyword' do
     visit "/pets"
+
     expect(page).to have_button("Search")
   end
 
   it 'lists partial matches as search results' do
-    shelter = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
-    pet_1 = Pet.create(adoptable: true, age: 7, breed: 'sphynx', name: 'Bare-y Manilow', shelter_id: shelter.id)
-    pet_2 = Pet.create(adoptable: true, age: 3, breed: 'domestic pig', name: 'Babe', shelter_id: shelter.id)
-    pet_3 = Pet.create(adoptable: true, age: 4, breed: 'chihuahua', name: 'Elle', shelter_id: shelter.id)
-
     visit "/pets"
 
-    fill_in 'Search', with: "Ba"
+    fill_in(:search, with: "Ba")
     click_on("Search")
 
-    expect(page).to have_content(pet_1.name)
-    expect(page).to have_content(pet_2.name)
-    expect(page).to_not have_content(pet_3.name)
+    expect(page).to have_content(@pet_1.name)
+    expect(page).to have_content(@pet_2.name)
+    expect(page).to_not have_content(@pet_3.name)
   end
-
-  
-# Then I see a link to "Start an Application"
-# When I click this link
-# Then I am taken to the new application page where I see a form
-# When I fill in this form with my:
-#   - Name
-#   - Street Address
-#   - City
-#   - State
-#   - Zip Code
-# And I click submit
-# Then I am taken to the new application's show page
-# And I see my Name, address information, and description of why I would make a good home
-# And I see an indicator that this application is "In Progress"
   
 describe 'User story 2' do  
   it 'has a link to start an application' do
-      shelter = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
-      pet_1 = Pet.create(adoptable: true, age: 7, breed: 'sphynx', name: 'Bare-y Manilow', shelter_id: shelter.id)
-      pet_2 = Pet.create(adoptable: true, age: 3, breed: 'domestic pig', name: 'Babe', shelter_id: shelter.id)
-      pet_3 = Pet.create(adoptable: true, age: 4, breed: 'chihuahua', name: 'Elle', shelter_id: shelter.id)
-
       visit '/pets'
 
-      expect(page).to have_link('START AN APPLICATION TODAY!', href: '/applications/new')
+      expect(page).to have_link('Start an Application', href: '/applications/new')
 
-      click_link 'START AN APPLICATION TODAY!'
+      click_link 'Start an Application'
 
       expect(current_path).to eq('/applications/new')
     end
