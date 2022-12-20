e3402fea478736f9731d91a8b104719283219bcf
Drew Layton <dlayton66@gmail.com>
2022-12-20 10:08:06 -0700

    refactor: Clean up tests

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -1,76 +1,28 @@
 require 'rails_helper'
+require 'test_helper'
 
 RSpec.describe "Application Show Page" do
   before :each do
-    @shelter_1 = Shelter.create!(
-      foster_program: true,
-      name: "Healthy Paws",
-      city: "Denver",
-      rank: 3
-    )
-
-    @application_1 = Application.create!(
-      name: "Bob",
-      street_address: "123 Leaf Street",
-      city: "Denver",
-      state: "CO",
-      zip_code: 80020
-    )
-
-    @pet_1 = Pet.create!(
-      name: "Pepper",
-      adoptable: true,
-      age: 4,
-      breed: "Pitbull",
-      shelter_id: @shelter_1.id
-    )
-
-    @pet_2 = Pet.create!(
-      name: "Pepperoni",
-      adoptable: true,
-      age: 2,
-      breed: "Corgi",
-      shelter_id: @shelter_1.id
-    )
-
-    @pet_3 = Pet.create!(
-      name: "Lemonpop",
-      adoptable: true,
-      age: 4,
-      breed: "Pitbull",
-      shelter_id: @shelter_1.id
-    )
+    seed_shelters
+    seed_applications
+    seed_pets
   end
 
   describe "User Story 1" do
     describe "User visits '/applications/:id'" do
       it 'has application attributes' do
-        pet_1 = @application_1.pets.create!(
-          name: "Pepper",
-          adoptable: true,
-          age: 4,
-          breed: "Pitbull",
-          shelter_id: @shelter_1.id
-        )
-          
-        pet_2 = @application_1.pets.create!(
-          name: "Daisy",
-          adoptable: true,
-          age: 14,
-          breed: "Beagle",
-          shelter_id: @shelter_1.id
-        )
+        ApplicationPet.create!(application: @application_1, pet:@pet_1)
 
         visit "/applications/#{@application_1.id}"
         
         expect(page).to have_content(@application_1.name)
         expect(page).to have_content(@application_1.full_address)
-        expect(page).to have_content(pet_1.name)
+        expect(page).to have_content(@pet_1.name)
         expect(page).to have_content(@application_1.status)
 
         click_link "Pepper"
 
-        expect(current_path).to eq("/pets/#{pet_1.id}")
+        expect(current_path).to eq("/pets/#{@pet_1.id}")
       end
     end
   end
@@ -120,13 +72,7 @@ RSpec.describe "Application Show Page" do
   describe "User Story 6" do
     describe "User has added pets to application" do
       it 'now has a section to enter good owner description' do
-        pet_1 = @application_1.pets.create!(
-          name: "Pepper",
-          adoptable: true,
-          age: 4,
-          breed: "Pitbull",
-          shelter_id: @shelter_1.id
-        )
+        ApplicationPet.create!(application: @application_1, pet:@pet_1)
 
         visit "/applications/#{@application_1.id}"
 
@@ -168,11 +114,6 @@ RSpec.describe "Application Show Page" do
     describe "Partial matches" do
       it 'can see any pet whos name partially matches the search' do
         visit "/applications/#{@application_1.id}"
-
-        expect(page).to have_field(:search)
-        expect(page).to have_content("Add a Pet to this Application")
-        expect(page).to have_no_content('Pepper')
-        expect(current_path).to eq("/applications/#{@application_1.id}")
         
         fill_in(:search, with: 'Pep')
         click_button('Search For Pet!')
@@ -188,11 +129,6 @@ RSpec.describe "Application Show Page" do
     describe "case insensitive" do
       it 'can match pet names despite case' do
         visit "/applications/#{@application_1.id}"
-
-        expect(page).to have_field(:search)
-        expect(page).to have_content("Add a Pet to this Application")
-        expect(page).to have_no_content('Pepper')
-        expect(current_path).to eq("/applications/#{@application_1.id}")
         
         fill_in(:search, with: 'pEpPER')
         click_button('Search For Pet!')
